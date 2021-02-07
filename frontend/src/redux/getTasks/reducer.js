2d54c3ad44880afe8aeb8fcf9da6e9c4c5b7d22d
IrinaPopova99 <janneteldenorr@gmail.com>
2021-02-07 09:02:49 +0700

    Add task completion

--- frontend/src/redux/getTasks/reducer.js
+++ frontend/src/redux/getTasks/reducer.js
@@ -26,24 +26,25 @@ export default function placesReducer(state = initialState, action) {
         tasks: [],
         error: action.payload.error,
       };
-    case types.TOGGLE_TASK_STARTED:
-      return {
-        ...state,
-        loading: true,
-      };
-    case types.TOGGLE_TASK_SUCCESS:
+    // case typesToggle.TOGGLE_TASK_STARTED:
+    //   return {
+    //     ...state,
+    //     loading: true,
+    //   };
+    case typesToggle.TOGGLE_TASK_SUCCESS:
+      console.log(`it is${state.tasks.map((todo) => (todo.name === action.payload ? { ...todo, done: !todo.done } : todo))}`)
       return {
         ...state,
         loading: false,
-        tasks: [...state.tasks, action.payload ],
+        tasks: state.tasks.map((todo) => (todo.name === action.payload ? { ...todo, done: !todo.done } : todo)),
         error: "",
       };
-    case types.TOGGLE_TASK_FAILURE:
-      return {
-        ...state,
-        loading: false,
-        error: action.payload.error,
-      };
+    // case typesToggle.TOGGLE_TASK_FAILURE:
+    //   return {
+    //     ...state,
+    //     loading: false,
+    //     error: action.payload.error,
+    //   };
     // case typesAdd.ADD_TASK_SUCCESS:
     //   return {
     //     loading: false,
--- frontend/src/redux/toggleTask/action.js
+++ frontend/src/redux/toggleTask/action.js
@@ -2,10 +2,14 @@
 import axios from "axios";
 import types from "../constants/typesToggle";
 
-const toggleTaskSuccess = (tasks) => ({
+const toggleTaskSuccess = (tasks) => {
+  console.log(`from success ${tasks}`)
+  return(
+  {
   type: types.TOGGLE_TASK_SUCCESS,
   payload: tasks,
 });
+}
 
 const toggleTaskStarted = () => ({
   type: types.TOGGLE_TASK_FAILURE,
@@ -26,9 +30,9 @@ const toggleTask = ({id, name}) => async (dispatch) => {
     .post(`https://remembrallbot.herokuapp.com/users/${id}/finish-task`, { name } ) 
     .then((res) => {
         console.log("res.data");
-        console.log(`(((( ${res.data}`);
+        console.log(`(((( ${res.data.name}`);
         console.log("res.data");
-      dispatch(toggleTaskSuccess(res.data));
+      dispatch(toggleTaskSuccess(res.data.name));
     })
     .catch((err) => {
       dispatch(toggleTaskFailure(err.message));
